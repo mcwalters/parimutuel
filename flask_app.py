fc2faa485b749bb282f5a0c6b61893b0b4a5392e
Brendan McWalters <mcwalters@gmail.com>
2017-04-22 00:41:57 +0000

    added database successifully

--- flask_app.py
+++ flask_app.py
@@ -2,18 +2,26 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, render_template, redirect, request
-from flask.ext.mysql import MySQL
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="mcwalters",
-    password="Fuzzy",
+    password="foobarFuzzy",
     hostname="mcwalters.mysql.pythonanywhere-services.com",
     databasename="mcwalters$parimutuel",
 )
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+
+db = SQLAlchemy(app)
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+
 
 @app.route('/')
 def hello_world():
@@ -26,6 +34,9 @@ def login():
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
+            comment = Comment(content=request.form["username"])
+            db.session.add(comment)
+            db.session.commit()
         else:
             return redirect('/')
     return render_template('login.html', error=error)
