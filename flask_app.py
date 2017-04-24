fd516b3863ae0c6f0e571b8ff5a4a1957c85296c
Brendan McWalters <mcwalters@gmail.com>
2017-04-24 22:04:54 +0000

    added new db concepts, prints df to main page c css formatting

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 
 from flask import Flask, render_template, redirect, request, session
 from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
 import os
 
 app = Flask(__name__)
@@ -23,12 +24,25 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
 
+class BankRoll(db.Model):
+    __tablename__ = "bank"
+    id = db.Column(db.Integer, primary_key=True)
+    owner = db.Column(db.String(4096))
+    balance = db.Column(db.Numeric(13,2))
+
+class Bet(db.Model):
+    __tablename__ = "bets"
+    id = db.Column(db.Integer, primary_key=True)
+    bettor = db.Column(db.String(4096))
+    in_progress = db.Column(db.Boolean())
+    bet = db.Column(db.Numeric(13,2))
 
 @app.route('/')
 def hello_world():
     if not session.get('logged_in'):
         return redirect('/login')
-    return 'Hello from Flaskeresque!'
+    return render_template('bets.html',tables=[pd.read_sql_query('select * from comments;',db.engine).to_html()],
+                           titles = ['na', 'Female surfers', 'Male surfers'])
 
 # route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
