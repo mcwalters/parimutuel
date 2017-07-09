c32e74a403d01e01a6c926aa2d39e4fd98b08d6e
Brendan McWalters <mcwalters@gmail.com>
2017-07-09 04:26:59 +0000

    made login work

--- flask_app.py
+++ flask_app.py
@@ -19,42 +19,61 @@ app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 
 db = SQLAlchemy(app)
 
-class Comment(db.Model):
-    __tablename__ = "comments"
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(4096))
-
 class BankRoll(db.Model):
     __tablename__ = "bank"
     id = db.Column(db.Integer, primary_key=True)
     owner = db.Column(db.String(4096))
+    password = db.Column(db.String(4096))
     balance = db.Column(db.Numeric(13,2))
+
+class Race(db.Model):
+    __tablename__ = "races"
+    id = db.Column(db.Integer, primary_key=True)
+    race_name = db.Column(db.String(4096))
+    in_progress = db.Column(db.Boolean())
 
 class Bet(db.Model):
     __tablename__ = "bets"
     id = db.Column(db.Integer, primary_key=True)
+    race = db.Column(db.String(4096))
     bettor = db.Column(db.String(4096))
-    in_progress = db.Column(db.Boolean())
     bet = db.Column(db.Numeric(13,2))
 
-@app.route('/')
-def hello_world():
-    if not session.get('logged_in'):
-        return redirect('/login')
-    return render_template('bets.html',tables=[pd.read_sql_query('select * from comments;',db.engine).to_html()],
+
+@app.route('/bets')
+def bet_list():
+    return render_template('bets.html',tables=[pd.read_sql_query('select * from bank;',db.engine).to_html()],
+                           titles = ['na', 'Female surfers', 'Male surfers'])
+
+@app.route('/races')
+def race_list():
+    return render_template('bets.html',tables=[pd.read_sql_query('select * from races where in_progress;',db.engine).to_html()],
                            titles = ['na', 'Female surfers', 'Male surfers'])
 
 # route for handling the login page logic
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-            comment = Comment(content=request.form["username"])
-            db.session.add(comment)
-            db.session.commit()
+        owner = request.form['username']
+        password = request.form['password']
+        if BankRoll.query.filter_by(owner=owner).first():
+            if BankRoll.query.filter_by(owner=owner,password=password).first():
+                session['logged_in'] = True
+                return redirect('/bets')
+            else:
+                error = 'Wrong password. Please try again.'
+                return render_template('login.html', error=error)
         else:
-            session['logged_in'] = True
-            return redirect('/')
+            new_bettor = BankRoll(owner=request.form["username"],
+                                  password=request.form["password"],
+                                  balance=100)
+            db.session.add(new_bettor)
+            db.session.commit()
+            return redirect('/races')
     return render_template('login.html', error=error)
+
+
+# For development to reset the database
+#db.drop_all()
+#db.create_all()
